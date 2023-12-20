60c8b4703e5bb6f0044e0701c8bc11a37d8acc3a
kimmykokonut <kimmykokonut@gmail.com>
2023-12-20 15:44:33 -0800

    eod
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail>

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -5,24 +5,6 @@ import React, { useState, useEffect } from "react";
 const UserDashboard = () => {
     // const [userInfo, setUserInfo] = useState({});
 
-    //display user info, name, email, quizzes taken, stats...
-
-    // useEffect(() => {
-    //     const fetchUserData = async () => {
-    //         try {
-    //             if (auth.currentUser){
-    //                 const userRecord = await getUserByEmail(getAuth(), auth.currentUser.email);
-    //                 console.log(`user data: ${userRecord.toJSON()}`);
-    //             }else{
-    //                 console.log("user not authenticated");
-    //             }
-    //         } catch (error) {
-    //             console.log(`error: ${error}`);
-    //         }
-    //     };
-    //     fetchUserData();
-    // }, []);
-    
     return(
         <React.Fragment>
         <h1>Dashboard</h1>
@@ -32,7 +14,7 @@ const UserDashboard = () => {
             <li>quiz name here, timestamp</li>
         </ul>
         <h3>Quizzes taken:
-            
+
         </h3>
         <ul>
             <li>quiz here. score here</li>
